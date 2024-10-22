afb0c72937a8e2fe67e654d81438d127bf3f39d3
Jolene <jolene.m.p.brown@gmail.com>
2024-10-22 16:03:14 +0100

    updated library

--- classes/Media.js
+++ classes/Media.js
@@ -1,4 +1,19 @@
 // create your Media class:
+class Media {
+    constructor(title, genre, year) {
+        this.title = title;
+        this.genre = genre;
+        this.year = year
+    }
+    static totalMediaCount = 0
+    summary() {
+        return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre}`
+    }
+}
+
+const book = new Media("Oliver Twist", "Nostalgia", 1870)
+Media.totalMediaCount;
+console.log(book.totalMediaCount);
 
 // uncomment below to export it:
 module.exports = Media;
